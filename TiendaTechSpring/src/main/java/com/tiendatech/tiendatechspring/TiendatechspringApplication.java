6ee707e4353bed6c6d123a1dbaa4c71aafad633f
matelat-daw <cesarmatelat@gmail.com>
2026-05-20 09:55:52 +0100

    Update Tienda Spring OK

--- TiendaTechSpring/src/main/java/com/tiendatech/tiendatechspring/TiendatechspringApplication.java
+++ TiendaTechSpring/src/main/java/com/tiendatech/tiendatechspring/TiendatechspringApplication.java
@@ -3,11 +3,12 @@ package com.tiendatech.tiendatechspring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.tiendatech.tiendatechspring.model.Producto;
+
 @SpringBootApplication
 public class TiendatechspringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TiendatechspringApplication.class, args);
 	}
-
 }
