59e670a9da29b8f14ea8191f0e9aa82154341246
matelat-daw <cesarmatelat@gmail.com>
2026-05-25 17:07:22 +0100

    Update Micro Servicios Clean Imports

--- Asociation/actividades_service/src/main/java/com/asociacion/actividades_service/controller/InscripcionController.java
+++ Asociation/actividades_service/src/main/java/com/asociacion/actividades_service/controller/InscripcionController.java
@@ -1,6 +1,5 @@
 package com.asociacion.actividades_service.controller;
 
-import javax.annotation.processing.Generated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
--- Asociation/actividades_service/src/main/java/com/asociacion/actividades_service/service/ActividadService.java
+++ Asociation/actividades_service/src/main/java/com/asociacion/actividades_service/service/ActividadService.java
@@ -3,7 +3,6 @@ package com.asociacion.actividades_service.service;
 import org.springframework.stereotype.Service;
 import com.asociacion.actividades_service.repository.ActividadesRepository;
 import com.asociacion.actividades_service.model.Actividad;
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
