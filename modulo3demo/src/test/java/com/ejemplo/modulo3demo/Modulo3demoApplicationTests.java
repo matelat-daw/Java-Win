f3d94f1cae3ec4451d7928edf1e7d463ae8fd3b7
matelat-daw <cesarmatelat@gmail.com>
2026-05-27 20:21:32 +0100

    Update Con la Versión de la Época de las Cavernas

--- modulo3demo/src/test/java/com/ejemplo/modulo3demo/Modulo3demoApplicationTests.java
+++ modulo3demo/src/test/java/com/ejemplo/modulo3demo/Modulo3demoApplicationTests.java
@@ -3,11 +3,10 @@ package com.ejemplo.modulo3demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// Reemplaza tu anotación actual por esta (asegúrate de importar tu clase de aplicación real):
+@SpringBootTest(classes = Modulo3demoApplication.class) 
 class Modulo3demoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
