371e5740e063f071cedabf2637a7d105ec36309a
matelat-daw <cesarmatelat@gmail.com>
2026-05-28 18:18:38 +0100

    Update Examen de Mañana

---
+++ Pruebas/centro-formacion/formacion-service/src/main/java/com/formacion/formacion_service/FormacionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.formacion.formacion_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormacionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormacionServiceApplication.class, args);
+	}
+
+}
---
+++ Pruebas/centro-formacion/formacion-service/src/main/java/com/formacion/formacion_service/model/Alumno.java
@@ -0,0 +1,24 @@
+package com.formacion.formacion_service.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "alumno")
+public class Alumno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String email;
+    @ManyToOne
+    @JoinColumn(name = "curso_id", nullable = false)
+    @JsonIgnoreProperties(value = "curso")
+    private Curso curso;
+}
