b5feed6b4d54990a7279ab13844b7cc3bcdfe169
matelat-daw <cesarmatelat@gmail.com>
2026-06-17 20:47:07 +0100

    Update ALL Up to Date

--- Pruebas-2/centro-formacion/formacion-service/src/main/java/com/formacion/formacion_service/repository/AlumnoRepository.java
+++ Pruebas-2/centro-formacion/formacion-service/src/main/java/com/formacion/formacion_service/repository/AlumnoRepository.java
@@ -2,7 +2,5 @@ package com.formacion.formacion_service.repository;
 
 import com.formacion.formacion_service.model.Alumno;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface AlumnoRepository extends JpaRepository<Alumno, Long> { }
